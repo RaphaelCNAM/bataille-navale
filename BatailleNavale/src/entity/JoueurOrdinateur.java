d00783356539feeb5ae8ac17806bc1456299b8a2
Raphael987 <raphael@prox-i.pf>
2024-06-05 14:03:18 -1000

    code et grilles jolies

--- BatailleNavale/src/entity/JoueurOrdinateur.java
+++ BatailleNavale/src/entity/JoueurOrdinateur.java
@@ -4,24 +4,24 @@ import java.util.List;
 import java.util.Random;
 
 public class JoueurOrdinateur extends Joueur {
-	
-	private List<Coordonnee> tires;
-	private char[][] grille = getGrille();
-	
+
+    private List<Coordonnee> tirs;
+    private char[][] grille = getGrille();
+
     public JoueurOrdinateur() {
         super();
     }
-    
-    public void autoTire(){
-    	int ligne = new Random().nextInt(9 - 1 + 1) + 1 ;
-    	int colonne = new Random().nextInt(9 - 1 + 1) + 1 ; 
-    	
-    	if (this.grille[ligne][colonne] == 'O') {
-            grille[ligne][colonne]='M'; // M pour miss (raté)
+
+    public void autoTire() {
+        int ligne = new Random().nextInt(9 - 1 + 1) + 1;
+        int colonne = new Random().nextInt(9 - 1 + 1) + 1;
+
+        if (this.grille[ligne][colonne] == 'O') {
+            grille[ligne][colonne] = 'M'; // M pour miss (raté)
         } else {
-            grille[ligne][colonne]='X'; // X pour hit (touché)
+            grille[ligne][colonne] = 'X'; // X pour hit (touché)
         }
     }
-    
+
     // Peut ajouter des méthodes spécifiques pour l'ordinateur si nécessaire
 }
